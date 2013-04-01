c8adeee16ab0bdf8ceb0edb6039118b106f90aa7
Will Jones <williamgdjones@gmail.com>
2013-04-01 18:44:59 +0100

    Switched sidebar and Gmap

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
         div: '#basic_map',
         lat: 51.5073346,
         lng: -0.1276831,
-        width: '500px',
-        height: '500px',
+        width: '600px',
+        height: '300px',
         zoom: 12,
         zoomControl: true,
         zoomControlOpt: {
